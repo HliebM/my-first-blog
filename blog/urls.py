b6ab51e8ac5ef966f31b6d00954ae57b5a61c951
HliebM <hmarchenko@thinkport.digital>
2024-02-13 12:27:41 +0100

    Ende

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post_new', views.post_new, name='post_new'),
+    path('post_edit', views.post_edit, name='post_edit'),
 ]
 
 
